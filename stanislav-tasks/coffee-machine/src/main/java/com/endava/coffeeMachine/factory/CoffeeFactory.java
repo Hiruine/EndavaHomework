ba5a6601f1484bd7650472138ac5f35428049e2a
rmadiudin <radu.madiudin@yahoo.com>
2018-05-02 18:17:03 +0300

    liquibase task

---
+++ stanislav-tasks/coffee-machine/src/main/java/com/endava/coffeeMachine/factory/CoffeeFactory.java
@@ -0,0 +1,46 @@
+package com.endava.coffeeMachine.factory;
+
+import com.endava.coffeeMachine.coffeeTypes.Coffee;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+interface Factory {
+
+    Coffee createCoffee(String coffeeType);
+
+    static Factory factory(Consumer<Builder> consumer) {
+        Map<String, Supplier<Coffee>> map = new HashMap<>();
+        consumer.accept(map::put);
+        return coffeeType -> map.getOrDefault(coffeeType, () -> {
+            throw new IllegalArgumentException("Unknown coffee type.");
+        }).get();
+    }
+
+    interface Builder {
+        void register(String name, Supplier<Coffee> supplier);
+    }
+}
+
+
+/*static Coffee createCoffee(String coffeeType) {
+        switch (coffeeType) {
+            case "americano": {
+                return new Americano(
+                        new Ingredients.Builder(180, 30).build(),
+                        BigDecimal.valueOf(10));
+            }
+            case "cappuccino": {
+                return new Americano(
+                        new Ingredients
+                                .Builder(60, 30)
+                                .milk(60)
+                                .cream(60)
+                                .build(),
+                        BigDecimal.valueOf(10));
+            }
+        }
+        return null;
+    }*/
